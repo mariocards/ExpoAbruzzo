780229d8c3d750a77ea1ed2ffac7e6d76e17f304
gullit <mario.cardarelli@gmail.com>
2015-06-05 10:06:28 +0200

    no message

--- js/views/pages/Itinerari/ItinerarioView.js
+++ js/views/pages/Itinerari/ItinerarioView.js
@@ -2,6 +2,7 @@ define(function(require) {
 
   var Backbone = require("backbone");
   var Itinerario = require("models/Itinerario");
+   var L = require("leaflet");
   var Utils = require("utils");
 
   var ItinerarioView = Utils.Page.extend({
@@ -14,39 +15,64 @@ define(function(require) {
 
       this.template = Utils.templates.itinerarioview;
       this.model.on('sync', this.render, this);
-      
+       this.listenTo(this, "sync", this.addMap);
     },
 
     id: "itinerarioview",
     className: "i-g page",
 
     events: {
-      "touchend #goToMap": "goToMap",
-      "touchend #back-button": "goBack" 
+     
+      "touchend #back-button": "goBack"
+      // "tap #map-button": "addMap"  
      
     },
+    
+    
+    addMap: function() {
+
+      // the center of the map is the address of the University of L'Aquila
+      var mapCenter = {
+        lat: 42.3676443,
+        lon: 13.3496695
+      };
+
+      var options = {
+        center: new L.LatLng(mapCenter.lat, mapCenter.lon),
+        zoom: 12
+      };
+
+      // create the map
+      var map = L.map('map', options);
+      // say thanks to Leaflet
+      map.attributionControl.setPrefix("Leaflet");
+
+      // create a marker and add it to the map
+      L.marker([mapCenter.lat, mapCenter.lon]).addTo(map);
+
+      // add a layer showing Open Street Map's tiles
+      var layer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; OpenStreetMap',
+        maxZoom: 20
+      });
+      map.addLayer(layer);
+    },
+ 
 
     render: function() {
       $(this.el).html(this.template(this.model.toJSON()));
       
+    
       return this;
     },
     
-     render2: function() {
-      $(this.el).html(this.template(this.model.attributes.toJSON()));
-      
-      return this;
-    },
+    
     goBack : function(){
         Backbone.history.navigate("itinerarilistview", {
         trigger: true
       });
-    },
-    goToMap: function(e) {
-      Backbone.history.navigate("map", {
-        trigger: true
-      });
     }
+    
   });
 
   return ItinerarioView;
